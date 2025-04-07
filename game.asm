5eebf219e35870e73c34b5b09504e49954d47efb
tarxton <89666132+tarxton@users.noreply.github.com>
2025-04-08 00:45:38 +0200

    zaba je animirana

--- game.asm
+++ game.asm
@@ -36,6 +36,11 @@ ProgramStart:
 	mov r2,#0x403
 	str r2,[r4]
 
+	; Initialize RAM variables
+    ldr r4, CurrentFrame_Ptr
+    mov r5, #0
+    strb r5, [r4]      ; Initialize CurrentFrame to 0
+
 	bl LoadBackground
 
 	mov r8,#112
@@ -66,7 +71,8 @@ WaitForInput:
     cmp r9,#0
     beq CheckDown
     sub r9,r9,#12
-    mov r6, #0
+    mov r6, #0          ; North
+
     b AfterDirectionCheck
 
 CheckDown:
@@ -75,7 +81,7 @@ CheckDown:
     cmp r9,#160-16
     beq CheckLeft
     add r9,r9,#12
-    mov r6, #1
+    mov r6, #1          ; South
     b AfterDirectionCheck
 
 CheckLeft:
@@ -84,7 +90,7 @@ CheckLeft:
     cmp r8,#0
     beq CheckRight
     sub r8,r8,#12
-    mov r6, #2
+    mov r6, #2          ; West
     b AfterDirectionCheck
 
 CheckRight:
@@ -93,16 +99,38 @@ CheckRight:
     cmp r8,#240-16
     beq AfterDirectionCheck
     add r8,r8,#12
-    mov r6, #3
+    mov r6, #3          ; East
 
 AfterDirectionCheck:
-    bl ShowSprite
+    ; Remove old sprite first
+    bl RemoveSprite
 
-    ; Small delay to prevent accidental double presses
-    mov r0,#0x3FFF
-Delay:
+    ; Set jump frame for movement
+    ldr r4, CurrentFrame_Ptr
+    mov r7, #1
+    strb r7, [r4]
+    bl ShowSprite       ; This will use current r6 value for direction
+
+    ; Small delay for jump frame
+    mov r0,#0x7FFF
+Delay1:
     subs r0,r0,#1
-    bne Delay
+    bne Delay1
+
+    ; Remove jump frame sprite
+    bl RemoveSprite
+
+    ; Reset to normal frame
+    ldr r4, CurrentFrame_Ptr
+    mov r7, #0
+    strb r7, [r4]
+    bl ShowSprite       ; This will use current r6 value for direction
+
+    ; Delay before next input
+    mov r0,#0xFFFF
+Delay2:
+    subs r0,r0,#1
+    bne Delay2
 
     b InfLoop
 
@@ -127,91 +155,158 @@ ShowSprite:
 	mul r2,r1,r9
 	add r10,r10,r2
 
-	cmp r6, #0
-	beq LoadNorth
-	cmp r6, #1
-	beq LoadSouth
-	cmp r6, #2
-	beq LoadWest
-	cmp r6, #3
-	beq LoadEast
+	; Load current animation frame
+    ldr r4, CurrentFrame_Ptr
+    ldrb r5, [r4]       ; r5 now contains current frame (0 or 1)
+
+    cmp r6, #0          ; Check if facing North
+    bne CheckSouth
+    cmp r5, #0          ; Check which frame
+    bne NorthJump
+    adr r4, sprite_literals
+    ldr r7, [r4, #0]    ; Load normal sprite
+    ldr r1, [r7]
+    b AfterLoadFrog
+
+NorthJump:
+	adr r4, sprite_literals
+	ldr r7, [r4, #4]
+	ldr r1, [r7]        ; Two-step load for Frame 1
 	b AfterLoadFrog
 
-LoadNorth:
-	ldr r1, FrogNorth_Literal
+CheckSouth:
+	cmp r6, #1          ; Check if facing South
+	bne CheckWest
+	cmp r5, #0          ; Check which frame
+	bne SouthJump
+	adr r4, sprite_literals
+	ldr r7, [r4, #8]
+	ldr r1, [r7]
 	b AfterLoadFrog
-LoadSouth:
-	ldr r1, FrogSouth_Literal
+
+SouthJump:
+	adr r4, sprite_literals
+	ldr r7, [r4, #12]
+	ldr r1, [r7]        ; Two-step load for Frame 1
 	b AfterLoadFrog
-LoadWest:
-	ldr r1, FrogWest_Literal
+
+CheckWest:
+	cmp r6, #2          ; Check if facing West
+	bne CheckEast
+	cmp r5, #0          ; Check which frame
+   	bne WestJump
+	adr r4, sprite_literals
+	ldr r7, [r4, #16]
+	ldr r1, [r7]
 	b AfterLoadFrog
-LoadEast:
-	ldr r1, FrogEast_Literal
+
+WestJump:
+	adr r4, sprite_literals
+	ldr r7, [r4, #20]
+	ldr r1, [r7]
+	b AfterLoadFrog
+
+CheckEast:
+	cmp r5, #0          
+   	bne EastJump
+	adr r4, sprite_literals
+	ldr r7, [r4, #24]
+	ldr r1, [r7]
+	b AfterLoadFrog
+
+EastJump:
+	adr r4, sprite_literals
+	ldr r7, [r4, #28]
+	ldr r1, [r7]
 
 AfterLoadFrog:
-	mov r6,#16
+
+.align 4
+sprite_literals:
+    .long FrogNorth_Literal
+    .long FrogNorthJump_Literal
+    .long FrogSouth_Literal
+    .long FrogSouthJump_Literal
+    .long FrogWest_Literal
+    .long FrogWestJump_Literal
+    .long FrogEast_Literal
+    .long FrogEastJump_Literal
+
+	mov r4,#16          ; Changed from r6 to r4
 Sprite_NextLine:
-	mov r5,#16
-	STMFD sp!,{r10}
+    mov r5,#16
+    STMFD sp!,{r10}
 Sprite_NextByte:
-	ldrH r3,[r1],#2
-	cmp r3,#0
-	beq Sprite_SkipPixel
-	strH r3,[r10]
+    ldrH r3,[r1],#2
+    cmp r3,#0
+    beq Sprite_SkipPixel
+    strH r3,[r10]
 Sprite_SkipPixel:
-	add r10,r10,#2
-	subs r5,r5,#1
-	bne Sprite_NextByte
-	LDMFD sp!,{r10}
-	add r10,r10,#240*2
-	subs r6,r6,#1
-	bne Sprite_NextLine
-	mov pc,lr
+    add r10,r10,#2
+    subs r5,r5,#1
+    bne Sprite_NextByte
+    LDMFD sp!,{r10}
+    add r10,r10,#240*2
+    subs r4,r4,#1      ; Changed from r6 to r4
+    bne Sprite_NextLine
+    mov pc,lr
 
 RemoveSprite:
-	STMFD sp!, {r0-r7, lr}
-	mov r10,#0x06000000
-	mov r1,#2
-	mul r2,r1,r8
-	add r10,r10,r2
-	mov r1,#240*2
-	mul r2,r1,r9
-	add r10,r10,r2
-	ldr r0, BackgroundData_Ptr
-	mov r1, r9
-	mov r2, #240
-	mul r3, r1, r2
-	add r3, r3, r8
-	mov r2, #2
-	mul r3, r2, r3
-	add r0, r0, r3
-	mov r6, #16
+    STMFD sp!, {r0-r7, lr}
+    mov r10,#0x06000000
+    mov r1,#2
+    mul r2,r1,r8
+    add r10,r10,r2
+    mov r1,#240*2
+    mul r2,r1,r9
+    add r10,r10,r2
+    ldr r0, BackgroundData_Ptr
+    mov r1, r9
+    mov r2, #240
+    mul r3, r1, r2
+    add r3, r3, r8
+    mov r2, #2
+    mul r3, r2, r3
+    add r0, r0, r3
+    mov r4, #16        ; Changed from r6 to r4 for the counter
 RemoveSprite_NextLine:
-	mov r5, #16
-	STMFD sp!,{r10, r0}
+    mov r5, #16
+    STMFD sp!,{r10, r0}
 RemoveSprite_NextByte:
-	ldrH r3,[r0],#2
-	strH r3,[r10],#2
-	subs r5,r5,#1
-	bne RemoveSprite_NextByte
-	LDMFD sp!,{r10, r0}
-	add r10,r10,#240*2
-	add r0, r0, #240*2
-	subs r6,r6,#1
-	bne RemoveSprite_NextLine
-	LDMFD sp!, {r0-r7, pc}
+    ldrH r3,[r0],#2
+    strH r3,[r10],#2
+    subs r5,r5,#1
+    bne RemoveSprite_NextByte
+    LDMFD sp!,{r10, r0}
+    add r10,r10,#240*2
+    add r0, r0, #240*2
+    subs r4,r4,#1      ; Changed from r6 to r4
+    bne RemoveSprite_NextLine
+    LDMFD sp!, {r0-r7, pc}
 
 BackgroundData_Ptr: .long BackgroundData
+
+CurrentFrame_Ptr:   .long 0x03000000
+;AnimDelay_Ptr:      .long 0x03000001
 
 FrogNorth_Data: .incbin "assets/frogs/frogNorth.img.bin"
 FrogSouth_Data: .incbin "assets/frogs/frogSouth.img.bin"
 FrogEast_Data:  .incbin "assets/frogs/frogEast.img.bin"
 FrogWest_Data:  .incbin "assets/frogs/frogWest.img.bin"
 
+FrogNorthJump_Data:  .incbin "assets/frogs/frogNorthJump.img.bin"  ; Jump frame for north
+FrogSouthJump_Data:  .incbin "assets/frogs/frogSouthJump.img.bin"  ; Jump frame for south
+FrogEastJump_Data:   .incbin "assets/frogs/frogEastJump.img.bin"   ; Jump frame for east
+FrogWestJump_Data:   .incbin "assets/frogs/frogWestJump.img.bin"   ; Jump frame for west
+
 FrogNorth_Literal: .long FrogNorth_Data
 FrogSouth_Literal: .long FrogSouth_Data
 FrogWest_Literal:  .long FrogWest_Data
 FrogEast_Literal:  .long FrogEast_Data
 
-BackgroundData: .incbin "assets/background.img.bin"
+FrogNorthJump_Literal: .long FrogNorthJump_Data
+FrogSouthJump_Literal: .long FrogSouthJump_Data
+FrogEastJump_Literal:  .long FrogEastJump_Data
+FrogWestJump_Literal:  .long FrogWestJump_Data
+
+BackgroundData: .incbin "assets/background.img.bin" 
