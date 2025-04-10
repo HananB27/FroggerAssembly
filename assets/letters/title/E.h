3c89375c5cbba1ca7ca0c07f46dcf808b1e1cf7f
tarxton <89666132+tarxton@users.noreply.github.com>
2025-04-10 23:33:29 +0200

    slova

---
+++ assets/letters/title/E.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(E)
+
+//======================================================================
+//
+//	E, 16x16@16, 
+//	+ bitmap not compressed
+//	Total size: 512 = 512
+//
+//	Time-stamp: 2025-04-10, 22:48:48
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_E_H
+#define GRIT_E_H
+
+#define EBitmapLen 512
+extern const unsigned short EBitmap[256];
+
+#endif // GRIT_E_H
+
+//}}BLOCK(E)
