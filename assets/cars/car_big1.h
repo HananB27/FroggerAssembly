0b8013cf17ce2d53e9320e355d21e8da56623f8f
HananB27 <hananbajramovic075@gmail.com>
2025-04-21 21:10:59 +0200

    add truck sprites

---
+++ assets/cars/car_big1.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(car_big1)
+
+//======================================================================
+//
+//	car_big1, 32x16@16, 
+//	+ bitmap not compressed
+//	Total size: 1024 = 1024
+//
+//	Time-stamp: 2025-04-12, 12:54:03
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_CAR_BIG1_H
+#define GRIT_CAR_BIG1_H
+
+#define car_big1BitmapLen 1024
+extern const unsigned short car_big1Bitmap[512];
+
+#endif // GRIT_CAR_BIG1_H
+
+//}}BLOCK(car_big1)
